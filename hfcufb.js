727e5de960941f6660dd975aa0879bdea9ec2a8f
achapin <aaron.chapin@gmail.com>
2023-03-24 23:12:23 +0100

    Stealing some stuff from the speis builder

--- hfcufb.js
+++ hfcufb.js
@@ -1,4 +1,121 @@
+var force = [];
+var leaders = [];
+var ships = [];
+var upgrades = [];
+
+function getUrl(url){
+	var req = new XMLHttpRequest();
+	req.open("GET",url,true);
+	return req;
+}
+
+function loadURL(url){
+	var req = getUrl(url);
+	req.send();
+	return new Promise(function(resolve, reject) {
+		req.onreadystatechange = function() {
+			if(req.readyState === 4)
+			{
+				if(req.status === 200)
+				{
+					resolve(JSON.parse(req.response));
+				}else{
+					reject();
+				}
+			}
+		};
+	});
+}
+
 function initialize()
 {
     alert("initialized");    
+}
+
+function saveForce() {
+    var directory = getSaveDirectory();
+    var forceName = document.getElementById("forceName").value + " : " + document.getElementById("forceCost").innerHTML;
+    if(directory.indexOf(forceName) < 0){
+        directory.push(forceName);
+    }
+    window.localStorage.setItem('forceDirectory', JSON.stringify(directory));
+    window.localStorage.setItem(forceName, JSON.stringify(force));
+    console.log("force saved as " + forceName);
+}
+
+function openForceWindow() {
+    var forceWindow = document.getElementById("forceWindow");
+    forceWindow.classList.remove("hidden")
+    var directory = getSaveDirectory();
+    var forceList = document.getElementById("forceList");
+    forceList.innerHTML = "";
+    directory.forEach(function(forceName) {
+        var entry = document.createElement("div");
+        entry.classList.add("forceEntry")
+        var name = document.createElement("span");
+        name.innerHTML = forceName;
+        entry.appendChild(name);
+        var load = document.createElement("button");
+        load.innerHTML = "Load";
+        load.onclick = function() {
+            loadForce(forceName);
+            closeForceWindow();
+        }
+        entry.appendChild(load);
+
+        var deleteForce = document.createElement("button");
+        deleteForce.innerHTML = "Delete";
+        deleteForce.onclick = function() {
+            window.localStorage.removeItem(forceName);
+            directory.splice(directory.indexOf(forceName), 1);
+            window.localStorage.setItem('forceDirectory', JSON.stringify(directory));
+            forceList.removeChild(entry);
+        }
+        entry.appendChild(deleteForce);
+
+        forceList.appendChild(entry);
+    });
+}
+
+function getSaveDirectory() {
+    var directory = JSON.parse(window.localStorage.getItem('forceDirectory'));
+    if(!directory) {
+        directory = [];
+    }
+    return directory;
+}
+
+function closeForceWindow() {
+    forceWindow.classList.add("hidden")
+}
+
+function shipsLoaded(json){
+	ships = json;
+}
+
+function leadersLoaded(json){
+	leaders = json;
+}
+
+function upgradesLoaded(json){
+	upgrades = json;
+}
+
+function initialize()
+{
+    var shipsLoadPromise = loadURL("data/ships.json");
+	shipsLoadPromise.then(shipsLoaded);
+	shipsLoadPromise.catch(function(){alert("ships data load failed");});
+
+    var leadersLoadPromise = loadURL("data/leaders.json");
+	leadersLoadPromise.then(leadersLoaded);
+	leadersLoadPromise.catch(function(){alert("leaders data load failed");});
+
+    var upgradesLoadPromise = loadURL("data/upgrades.json");
+	upgradesLoadPromise.then(upgradesLoaded);
+	upgradesLoadPromise.catch(function(){alert("upgrades data load failed");});
+
+    Promise.all([shipsLoadPromise, leadersLoadPromise, upgradesLoadPromise]).then(_ => {
+        alert("all data loaded");
+    });
 }
