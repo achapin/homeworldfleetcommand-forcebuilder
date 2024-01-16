8abab4229f7f676e6bdb1a16c0d0e9a45f52223c
Klaus Kopec <kkopec@salesforce.com>
2024-01-16 21:58:41 +0100

    Moved Military Outpost from ships to planets data file
    This required showing the "Planets" selector also if the campaign
    expansion was not activated (i.e., its checkbox is unchecked).
    
    This required that all planets have a "source" field, which was added
    with source="campaign" for the 4 campaign expansion planets. The
    Military Outpost already had this from it's time as a unit.
    
    This addresses issue #21

--- hfcufb.js
+++ hfcufb.js
@@ -1151,19 +1151,19 @@ function setupOptions(){
     unitSection.appendChild(unitAddButton);
 
     var planetSection = document.getElementById("addPlanetSection");
-    if(!contentCampaign.checked){
-        planetSection.classList.add("noDisplay");
-    } else {
-        planetSection.classList.remove("noDisplay")
-    }
+
     var planetLabel = document.createElement("span");
     planetLabel.innerHTML = "Planets:";
     planetSection.appendChild(planetLabel);
 
     planetDropdown = document.createElement("SELECT");
     planets.forEach(function(planet){
-        var option = new Option(displayText[planet.name] + " (" + planet.cost + ")", planet.name);
-        planetDropdown.add(option);
+
+         // Military Outpost is the only base game planet, so we only show that if no additional content is activated
+        if(validSource(planet)){
+            var option = new Option(displayText[planet.name] + " (" + planet.cost + ")", planet.name);
+            planetDropdown.add(option);
+        }
     });
     planetSection.appendChild(planetDropdown);
 
